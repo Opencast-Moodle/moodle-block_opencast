88ab161a8c445b7b35e702ffcd0e555ab9c64e4d
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2018-08-29 15:37:39 +0200

    Added database table for deletejobs

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018082700;
+$plugin->version = 2018082800;
 $plugin->requires = 2017051500;
 $plugin->maturity  = MATURITY_BETA;
 $plugin->release   = 'v3.4-r1'; // Developed under Moodle 3.4. First release.
