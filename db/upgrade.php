c7c40b6c720979f3ad3a7df8922800e9ea2b140f
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2018-08-30 17:30:03 +0200

    Added new table to save group access

--- db/upgrade.php
+++ db/upgrade.php
@@ -168,5 +168,29 @@ function xmldb_block_opencast_upgrade($oldversion) {
         upgrade_block_savepoint(true, 2018082800, 'opencast');
     }
 
+    if ($oldversion < 2018082802) {
+
+        // Define table block_opencast_groupaccess to be created.
+        $table = new xmldb_table('block_opencast_groupaccess');
+
+        // Adding fields to table block_opencast_groupaccess.
+        $table->add_field('id', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, XMLDB_SEQUENCE, null);
+        $table->add_field('opencasteventid', XMLDB_TYPE_CHAR, '100', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('groups', XMLDB_TYPE_TEXT, null, null, XMLDB_NOTNULL, null, null);
+        $table->add_field('timecreated', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+        $table->add_field('timemodified', XMLDB_TYPE_INTEGER, '10', null, XMLDB_NOTNULL, null, null);
+
+        // Adding keys to table block_opencast_groupaccess.
+        $table->add_key('primary', XMLDB_KEY_PRIMARY, array('id'));
+
+        // Conditionally launch create table for block_opencast_groupaccess.
+        if (!$dbman->table_exists($table)) {
+            $dbman->create_table($table);
+        }
+
+        // Opencast savepoint reached.
+        upgrade_block_savepoint(true, 2018082802, 'opencast');
+    }
+
     return true;
 }
