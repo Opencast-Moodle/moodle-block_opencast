77a3d429d2f64bd744e8249c04ac52b0c4578ab2
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2018-10-29 15:56:35 +0100

    Release v3.5-r1

--- version.php
+++ version.php
@@ -23,9 +23,9 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2018091700;
+$plugin->version = 2018102900;
 $plugin->requires = 2017051500;
 $plugin->maturity  = MATURITY_BETA;
-$plugin->release   = 'v3.4-r1'; // Developed under Moodle 3.4. First release.
+$plugin->release   = 'v3.5-r1'; // First release for Moodle 3.5.
 $plugin->component = 'block_opencast';
 $plugin->dependencies = array('tool_opencast' => 2018091700);
