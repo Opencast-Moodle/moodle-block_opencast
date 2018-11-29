df01a3600251b4edc157221c0cf9f89281caf8a2
Tobias Reischmann <tobias.reischmann@wi.uni-muenster.de>
2018-11-29 16:56:35 +0100

    Moved upgrade steps from three different if statements to one

--- db/upgrade.php
+++ db/upgrade.php
@@ -125,12 +125,6 @@ function xmldb_block_opencast_upgrade($oldversion) {
             $dbman->drop_table($table);
         }
 
-        // Opencast savepoint reached.
-        upgrade_block_savepoint(true, 2018082800, 'opencast');
-    }
-
-    if ($oldversion < 2018082800) {
-
         // Define field permanent to be added to block_opencast_roles.
         $table = new xmldb_table('block_opencast_roles');
         $field = new xmldb_field('permanent', XMLDB_TYPE_INTEGER, 1, true, XMLDB_NOTNULL, null, 1, 'actions');
@@ -139,12 +133,6 @@ function xmldb_block_opencast_upgrade($oldversion) {
         if (!$dbman->field_exists($table, $field)) {
             $dbman->add_field($table, $field);
         }
-
-        // Opencast savepoint reached.
-        upgrade_block_savepoint(true, 2018082800, 'opencast');
-    }
-
-    if ($oldversion < 2018082800) {
 
         // Define table block_opencast_deletejob to be created.
         $table = new xmldb_table('block_opencast_deletejob');
