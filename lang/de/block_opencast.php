ebdf3ce5543c06556216d7895948ff043c00a874
Amrita1991 <amritad1991@gmail.com>
2020-04-28 14:16:15 +0200

    de lang file with dropdown translations
    the lang file has two translation strings language_params and license_params for the dropdown list item translated in German

---
+++ lang/de/block_opencast.php
@@ -0,0 +1,27 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Strings for component 'block_opencast', language 'en'
+ *
+ * @package    block_opencast
+ * @copyright  2017 Andreas Wagner, SYNERGY LEARNING
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+$string['language_params'] = '{"":"keine Option ausgewählt","slv":"Slowenisch","por":"Portugiesisch","roh":"Romanisch","ara":"Arabisch","pol":"Polnisch","ita":"Italienisch","zho":"Chinesisch","fin":"Finnisch","dan":"Dänisch","ukr":"Ukrainisch","fra":"Französisch","spa":"Spanisch","gsw":"Schweizerdeutsch","nor":"Norwegisch","rus":"Russisch","jpx":"Japanisch","nld":"Niederländisch","tur":"Türkisch","hin":"Hindi","swa":"Schwedisch","eng":"Englisch","deu":"Deutsch"}';
+
+$string['license_params'] = '{"":"keine Option ausgewählt","ALLRIGHTS":"Alle Rechte vorbehalten","CC0":"CC0","CC-BY-ND":"CC BY-ND","CC-BY-NC-ND":"CC BY-NC-ND","CC-BY-NC-SA":"CC BY-NC-SA","CC-BY-SA":"CC BY-SA","CC-BY-NC":"CC BY-NC","CC-BY":"CC BY"}';
+?>
