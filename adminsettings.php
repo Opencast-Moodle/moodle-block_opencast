87827cdff2258ab2dc5590260dbf8bf1d041b899
Alexander Bias <alexander.bias@uni-ulm.de>
2020-10-25 19:34:16 +0100

    Build full admin settings pages only if necessary
    This patch improves the admin settings pages in a way that they are only built if they are really needed.

--- adminsettings.php
+++ adminsettings.php
@@ -33,7 +33,7 @@ require_login();
 $PAGE->set_url('/blocks/opencast/adminsettings');
 
 if (has_capability('moodle/site:config', context_system::instance())) {
-    admin_externalpage_setup('block_opencast');
+    admin_externalpage_setup('block_opencast_generalsettings');
 
     $mform = new block_opencast\admin_form();
 
