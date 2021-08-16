3e50a5a2801a6cdf1fa772dd07e6306f8e2a8f97
Tamara Gunkel <tamara.gunkel@web.de>
2021-08-16 17:30:39 +0200

    update tool dependency

--- version.php
+++ version.php
@@ -28,4 +28,4 @@ $plugin->requires = 2017111300;
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->release   = 'v3.11-r2';
 $plugin->component = 'block_opencast';
-$plugin->dependencies = array('tool_opencast' => 2021062500);
+$plugin->dependencies = array('tool_opencast' => 2021080100);
