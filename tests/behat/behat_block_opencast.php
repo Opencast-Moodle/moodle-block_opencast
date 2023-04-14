0ad57769b631603e3c7d0f3959682ad5774e49cd
farbod <zamanifarbod2@gmail.com>
2023-04-14 17:33:05 +0200

    checker fix p.3

--- tests/behat/behat_block_opencast.php
+++ tests/behat/behat_block_opencast.php
@@ -140,6 +140,7 @@ class behat_block_opencast extends behat_base {
     public function breakpoint() {
         fwrite(STDOUT, "\033[s    \033[93m[Breakpoint] Press \033[1;93m[RETURN]\033[0;93m to continue...\033[0m");
         while (fgets(STDIN, 1024) == '') {
+            continue;
         }
         fwrite(STDOUT, "\033[u");
         return;
