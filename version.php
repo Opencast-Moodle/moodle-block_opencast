d7b14d5b424dd95cff271ccaef72aaddafdbe221
farbod <zamanifarbod2@gmail.com>
2023-05-11 21:37:08 +0200

    version bump and moodle-ci cleanup

--- version.php
+++ version.php
@@ -25,9 +25,9 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_opencast';
 $plugin->release = 'development-version';
-$plugin->version = 2023051102;
+$plugin->version = 2023051200;
 $plugin->requires = 2020061500; // Requires Moodle 3.9+.
 $plugin->maturity = MATURITY_ALPHA;
 $plugin->dependencies = array(
-    'tool_opencast' => 2023030100
+    'tool_opencast' => 2023051200
 );
