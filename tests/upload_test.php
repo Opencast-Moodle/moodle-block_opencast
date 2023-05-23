c77918e34ba1ef58a537d04f7371c0b9c2b5df64
farbod <zamanifarbod2@gmail.com>
2023-05-23 11:55:23 +0200

    improve phpunit test upload

--- tests/upload_test.php
+++ tests/upload_test.php
@@ -40,11 +40,14 @@ global $CFG;
 class upload_test extends advanced_testcase {
 
     /** @var string Test api url. */
-    private $apiurl = 'http://127.0.0.1:8080';
+    // private $apiurl = 'http://127.0.0.1:8080';
+    private $apiurl = 'https://moodle-opencast.opencast-niedersachsen.de';
     /** @var string Test api username. */
-    private $apiusername = 'admin';
+    // private $apiusername = 'admin';
+    private $apiusername = 'moodle';
     /** @var string Test api password. */
-    private $apipassword = 'opencast';
+    // private $apipassword = 'opencast';
+    private $apipassword = '6dwt5qbuXEBkvsu';
     /** @var int the curl timeout in milliseconds */
     private $apitimeout = 2000;
     /** @var int the curl connecttimeout in milliseconds */
@@ -125,15 +128,16 @@ class upload_test extends advanced_testcase {
         $apibridge = \block_opencast\local\apibridge::get_instance(1, true);
 
         $uploadhelper = new \block_opencast\local\upload_helper();
-        // Prevent mtrace output, which would be considered risky.
-        ob_start();
-        // Upload the file.
-        $uploadhelper->cron();
-        sleep(25);
-        $uploadhelper->cron();
-        sleep(25);
-        $uploadhelper->cron();
-        ob_end_clean();
+        $isuploaded = false;
+        $limiter = 5;
+        $counter = 0;
+        do {
+            $isuploaded = $this->notest_check_uploaded_video($course->id, $apibridge);
+            $counter++;
+            if ($counter >= $limiter) {
+                break;
+            }
+        } while (!$isuploaded);
 
         // Check if video was uploaded.
         $videos = $apibridge->get_course_videos($course->id);
@@ -142,4 +146,22 @@ class upload_test extends advanced_testcase {
         $this->assertCount(1, $videos->videos);
     }
 
+    /**
+     * Checks, if the video is available after upload, by running cron first to make sure upload video took place successfully.
+     *
+     * @param int $courseid Course ID
+     * @param \block_opencast\local\apibridge the apibridge instance
+     *
+     * @return bool true if the video is avialable, false otherwise.
+     */
+    private function notest_check_uploaded_video($courseid, \block_opencast\local\apibridge $apibridge) {
+        $uploadhelper = new \block_opencast\local\upload_helper();
+        // Prevent mtrace output, which would be considered risky.
+        ob_start();
+        $uploadhelper->cron();
+        ob_end_clean();
+        sleep(15);
+        $videos = $apibridge->get_course_videos($courseid);
+        return (!empty($videos->videos)) ? true : false;
+    }
 }
