9f3aaee08c382f3b6b1f08fb3a2c30eb89208dbf
NinaHerrmann <nina.herrmann@uni-muenster.de>
2023-11-22 21:32:50 +0100

    chaneg back to support 3.9+

--- version.php
+++ version.php
@@ -26,7 +26,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'block_opencast';
 $plugin->release = 'v4.3-r1';
 $plugin->version = 2023112200;
-$plugin->requires = 2022112800; // Requires Moodle 4.1+.
+$plugin->requires = 2020061524; // Requires Moodle 3.9+.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
     'tool_opencast' => 2023112200,
