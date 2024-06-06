e81a56d7213a7b0aa0606d3a7bb0d6f302410882
Nina Herrmann <nina.herrmann@uni-muenster.de>
2024-06-06 17:45:45 +0200

    Update version.php

--- version.php
+++ version.php
@@ -24,10 +24,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'block_opencast';
-$plugin->release = 'v4.3-r2';
+$plugin->release = 'v4.4-r1';
 $plugin->version = 2024060400;
 $plugin->requires = 2022112800; // Requires Moodle 4.1+.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
-    'tool_opencast' => 2023112200,
+    'tool_opencast' => 2024060400,
 ];
