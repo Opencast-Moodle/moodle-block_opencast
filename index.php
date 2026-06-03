a35d18527bfbd613540e0748a4ecea7ae384058b
FarbodZamani <53179227+ferishili@users.noreply.github.com>
2026-06-03 14:38:29 +0200

    fix condition for studio

--- index.php
+++ index.php
@@ -302,8 +302,8 @@ if (has_capability('block/opencast:addvideo', $coursecontext) && $SITE->id != $c
         // If LTI credentials are given or JWT is enabled, redirect ot recordvideo page.
         // If not, directly forward to Opencast studio.
         if (
-            empty($apibridge->get_lti_consumerkey()) ||
-            !($apibridge->api?->jwtservice?->is_enabled() ?? false)
+            !($apibridge->api?->jwtservice?->is_enabled() ?? false) &&
+            empty($apibridge->get_lti_consumerkey())
         ) {
             if (empty(get_config('block_opencast', 'opencast_studio_baseurl_' . $ocinstanceid))) {
                 $endpoint = settings_api::get_apiurl($ocinstanceid);
